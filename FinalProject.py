99317eabc2d9b81b298e7f5d10dfeac29e82c4fb
Marc Kielnik <marck7170133@gmail.com>
2024-07-15 12:16:24 -0500

    Etl change

--- FinalProject.py
+++ FinalProject.py
@@ -2,23 +2,26 @@
 import pandas as pd
 import glob
 
-def extract():
-    #For each file type: Create a temporary dataframe and concatenate to the 'data' dataframe.
-    for csvfile in glob.glob('*.csv'):
-        tmp_df = pd.read_csv(csvfile)
-        data = pd.concat([tmp_df], ignore_index=True)
+# Load the CSV files
+csv0 = pd.read_csv("ids_0.csv")
+csv1 = pd.read_csv("ids_1.csv")
+csv2 = pd.read_csv("ids_2.csv")
+combined = pd.concat([ csv0 , csv1 , csv2], ignore_index=True)
 
-    for jsonfile in glob.glob('*.json'):
-        tmp_df = pd.read_json(jsonfile,lines=True)
-        data = pd.concat([data, tmp_df], ignore_index=True)
+# Load the JSON files
+json3 = pd.read_json("ids_3.json", lines=True)
+json4 = pd.read_json("ids_4.json", lines=True)
+json7 = pd.read_json("ids_7.json", lines=True)
+json9 = pd.read_json("ids_9.json", lines=True)
+json10 = pd.read_json("ids_10.json", lines=True)
+combined = pd.concat([combined , json3 , json4 , json7 , json9 , json10], ignore_index=True)
 
-    for parquetfile in glob.glob('*.parquet'):
-        tmp_df = pd.read_parquet(parquetfile)
-        data = pd.concat([data,tmp_df], ignore_index=True)
-    
-    #Return combined data.
-    return data
+# Load the Parquet files
+parquet5 = pd.read_parquet("ids_5.parquet")
+parquet6 = pd.read_parquet("ids_6.parquet")
+parquet8 = pd.read_parquet("ids_8.parquet")
+parquet11 = pd.read_parquet("ids_11.parquet")
+combined = pd.concat([combined , parquet5 , parquet6 , parquet8 , parquet11], ignore_index=True)
 
-data = extract()
-print('data shape:', data.shape)
-print(data)
+# Display the combined DataFrame
+print(combined)
