c00536b5331ea04af569e8e1950f7feffbcb28b5
latorres19 <latorres19@neiu.edu>
2024-07-21 14:59:12 -0500

    AllOptions

--- FinalProject.py
+++ FinalProject.py
@@ -10,6 +10,7 @@ from sklearn.feature_selection import r_regression, f_regression, mutual_info_re
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.feature_selection import SelectKBest, SelectPercentile
+from xgboost import XGBRegressor
 
 #Extract section
 def extract():
@@ -112,34 +113,81 @@ X_test_standard = scaler.fit_transform(X_test)
 
 lr = LinearRegression()
 
-#Without Feature Selection
-rf_regressor = RandomForestRegressor(n_estimators=5)
+#WITHOUT FEATURE SELECTION
+rf_regressor = RandomForestRegressor(n_estimators=70)
 # train the model
 rf_regressor.fit(X_train_standard, y_train.ravel())
 # make predictions
 pred = rf_regressor.predict(X_test_standard)
 # compute r2-score and mse
 r2 = r2_score(y_test, pred)
-print("r2 score: {:.3f}".format(r2))
+print("r2 score WITHOUT: {:.3f}".format(r2))
 # compute mse
 mse = mean_squared_error(y_test, pred)
 print("mse: {:.3f}".format(mse))
 
-# Keep the best  30% of the features
+# SELECT PERCENTILE FEATURES
 r_selection = SelectPercentile(r_regression, percentile=30)
 # model fitting and feature selection
-X_train_selected = r_selection.fit_transform(X_train_standard, y_train)
+X_train_selected = r_selection.fit_transform(X_train_standard, y_train.ravel())
 X_test_selected = r_selection.transform(X_test_standard)
 print('X_train_selected shape:', X_train_selected.shape)
 print('X_test_selected shape:', X_test_selected.shape)
 # fit the regressor using the selected features
-rf_regressor.fit(X_train_selected, y_train)
-
+rf_regressor.fit(X_train_selected, y_train.ravel())
+print('X_train_selected shape:', X_train_selected.shape)
+print('X_test_selected shape:', X_test_selected.shape)
 # make predictions
 pred = rf_regressor.predict(X_test_selected)
 # compute r2-score and mse
 r2 = r2_score(y_test, pred)
-print("r2 score: {:.3f}".format(r2))
+print("r2 score PERCENTILE: {:.3f}".format(r2))
+# compute mse
+mse = mean_squared_error(y_test, pred)
+print("mse: {:.3f}".format(mse))
+
+
+# SELECT K-BEST
+r_selection = SelectKBest(r_regression, k=20)
+# use the training set to train the model, then transform the training set
+X_train_selected = r_selection.fit_transform(X_train_standard, y_train.ravel())
+# transform the test set
+X_test_selected = r_selection.transform(X_test_standard)
+print('X_train_selected shape:', X_train_selected.shape)
+print('X_test_selected shape:', X_test_selected.shape)
+# fit the random forest regressor using the selected features
+rf_regressor.fit(X_train_selected, y_train.ravel())
+# make predictions
+pred = rf_regressor.predict(X_test_selected)
+## different runs may lead to different results
+# compute r2-score and mse
+r2 = r2_score(y_test, pred)
+print("r2 score K-BEST: {:.3f}".format(r2))
+# compute mse
+mse = mean_squared_error(y_test, pred)
+print("mse: {:.3f}".format(mse))
+
+# XGBOOST
+xgb_regressor = XGBRegressor(n_estimators=100, max_depth=10)
+# fit the model using the entire dataset
+xgb_regressor.fit(X_train_standard, y_train.ravel())
+# display feature importance
+xgb_regressor.feature_importances_
+# store feature importance in a variable
+feature_importance = xgb_regressor.feature_importances_
+# preform feature selecting based on a 0.01 feature importance threshold
+X_train_selected = X_train_standard[:, feature_importance > 0.01]
+X_test_selected = X_test_standard[:, feature_importance > 0.01]
+print('X_train_selected shape:', X_train_selected.shape)
+print('X_test_selected shape:', X_test_selected.shape)
+# model fitting using the training dataset with selected features
+xgb_regressor.fit(X_train_selected, y_train.ravel())
+# make predictions
+pred = xgb_regressor.predict(X_test_selected)
+# regularization has a great improvement boost
+# compute r2-score and mse
+r2 = r2_score(y_test, pred)
+print("r2 score XGB: {:.3f}".format(r2))
 
 # compute mse
 mse = mean_squared_error(y_test, pred)
