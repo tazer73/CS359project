3ca67f415c5e94ae3d67d26170b463abbe059157
Marc Kielnik <marck7170133@gmail.com>
2024-07-22 16:30:11 -0500

    remove graph and add commanets

--- RevisedProject.py
+++ RevisedProject.py
@@ -118,20 +118,13 @@ xgbC.fit(X_train_selected , y_train)
 X_train_selected_pred = xgbC.predict(X_train_selected)
 X_test_selected_pred = xgbC.predict(X_test_selected)
 
-plt.plot(X_test_selected_pred[:100], label='X_test_selected_pred' , linestyle='None', marker='o', c='blue')
-plt.plot(y_test[:100],label='y_test' , linestyle='None', marker='+', c='red', markersize=10)
-plt.xlabel("X_test_selected_pred")
-plt.ylabel("y_test")
-plt.legend()
-plt.savefig(fname='LinearTest.png', format='png', dpi=600)
-plt.show()
-
+#tally the results
 acc_perc = accuracy_score(y_test, X_test_selected_pred)
 f1score_perc = f1_score(y_test, X_test_selected_pred)
 precision_perc = precision_score(y_test, X_test_selected_pred)
 recall_perc = recall_score(y_test, X_test_selected_pred)
 
-print('Perceptron Model')
+print('XGBC Model')
 print('-'*20)
 print('Accuracy: {:.3f}'.format(acc_perc))
 print('Precision: {:.3f}'.format(precision_perc))
